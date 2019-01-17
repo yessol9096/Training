7f41a2d093f607af5481e6e26641836ab3d18b2d
yessol9096 <yessol9096@naver.com>
2019-01-17 18:55:14 +0900

    계산기 UI

---
+++ Calculator/Calculator/Assets/Scripts/CalculatorMgr.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculatorMgr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void Input()
+	{
+		
+	}
+
+	void Calculate()
+	{
+		
+	}
+
+	void Print()
+	{
+		
+	}
+
+
+	public void OnClick_b0()
+	{
+
+	}
+	public void OnClick_b1()
+	{
+
+	}
+	public void OnClick_b2()
+	{
+
+	}
+	public void OnClick_b3()
+	{
+
+	}
+	public void OnClick_b4()
+	{
+
+	}
+	public void OnClick_b5()
+	{
+
+	}
+	public void OnClick_b6()
+	{
+
+	}
+	public void OnClick_b7()
+	{
+
+	}
+	public void OnClick_b8()
+	{
+
+	}
+	public void OnClick_b9()
+	{
+
+	}
+	public void OnClick_b_dot()
+	{
+
+	}
+	public void OnClick_b_plus()
+	{
+
+	}
+	public void OnClick_b_minus()
+	{
+
+	}
+	public void OnClick_b_mul()
+	{
+
+	}
+	public void OnClick_b_div()
+	{
+
+	}
+	public void OnClick_b_equal()
+	{
+
+	}
+	public void OnClick_b_clear()
+	{
+
+	}
+	public void OnClick_b_ac()
+	{
+
+	}
+}
