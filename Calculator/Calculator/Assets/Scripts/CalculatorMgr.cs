b8aec76243a46e9ba30532caea9ee58be5a8355e
yessol9096 <yessol9096@naver.com>
2019-01-18 19:12:50 +0900

    button 함수 추가

--- Calculator/Calculator/Assets/Scripts/CalculatorMgr.cs
+++ Calculator/Calculator/Assets/Scripts/CalculatorMgr.cs
@@ -7,7 +7,7 @@ public class CalculatorMgr : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+		
     }
 
     // Update is called once per frame
@@ -32,76 +32,24 @@ public class CalculatorMgr : MonoBehaviour
 	}
 
 
-	public void OnClick_b0()
+	public void OnClick_Numbutton(int button)
 	{
 
 	}
-	public void OnClick_b1()
+
+	public void OnClick_Calculatebutton(string button)
 	{
 
 	}
-	public void OnClick_b2()
+
+	public void OnClick_Equalbutton()
 	{
 
 	}
-	public void OnClick_b3()
-	{
 
-	}
-	public void OnClick_b4()
-	{
+    public void OnClick_Clearbutton(string button)
+    {
 
-	}
-	public void OnClick_b5()
-	{
+    }
 
-	}
-	public void OnClick_b6()
-	{
-
-	}
-	public void OnClick_b7()
-	{
-
-	}
-	public void OnClick_b8()
-	{
-
-	}
-	public void OnClick_b9()
-	{
-
-	}
-	public void OnClick_b_dot()
-	{
-
-	}
-	public void OnClick_b_plus()
-	{
-
-	}
-	public void OnClick_b_minus()
-	{
-
-	}
-	public void OnClick_b_mul()
-	{
-
-	}
-	public void OnClick_b_div()
-	{
-
-	}
-	public void OnClick_b_equal()
-	{
-
-	}
-	public void OnClick_b_clear()
-	{
-
-	}
-	public void OnClick_b_ac()
-	{
-
-	}
 }
